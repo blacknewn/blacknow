6259747bf9c93d0d1a5431a05e1879e754e4e431
blacknewn <targetingsudo@gmail.com>
2017-04-13 08:27:24 +0430

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -1099,11 +1099,11 @@ end
   if is_sudo(msg) then
 	if (matches[1]:lower() == "sendfile" or matches[1] == 'ارسال فایل') and matches[2] and matches[3] then
 		local send_file = "./"..matches[2].."/"..matches[3]
-		tdcli.sendDocument(msg.chat_id_, msg.id_,0, 1, nil, send_file, '@BeyondTeam', dl_cb, nil)
+		tdcli.sendDocument(msg.chat_id_, msg.id_,0, 1, nil, send_file, '@GODILOVEYOUME2', dl_cb, nil)
 	end
 	if matches[1]:lower() == "sendplug" or matches[1] == 'ارسال پلاگین' and matches[2] then
 	    local plug = "./plugins/"..matches[2]..".lua"
-		tdcli.sendDocument(msg.chat_id_, msg.id_,0, 1, nil, plug, '@BeyondTeam', dl_cb, nil)
+		tdcli.sendDocument(msg.chat_id_, msg.id_,0, 1, nil, plug, '@GODILOVEYOUME2', dl_cb, nil)
     end
   end
 
@@ -1197,7 +1197,7 @@ if matches[1] == "helptools" or  matches[1] == "راهنما ابزار" and is_
 if not lang then
 text = [[
 
-_Sudoer And Admins Beyond Bot Help :_
+_Sudoer And Admins Black Bot Help :_
 
 *!visudo* `[username|id|reply]`
 _Add Sudo_
@@ -1309,7 +1309,7 @@ tdcli.sendMessage(msg.chat_id_, 0, 1, text, 1, 'md')
 else
 
 text = [[
-_راهنمای ادمین و سودو های ربات بیوند:_
+_راهنمای ادمین و سودو های ربات بلک:_
 
 *سودو* `[username|id|reply]`
 _اضافه کردن سودو_
